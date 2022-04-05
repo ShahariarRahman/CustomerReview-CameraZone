6c5f1a33e569c53d68144f09c7146bd963ca3b07
ShahariarRahman <shahariarrahman98@gmail.com>
2022-04-05 23:51:10 +0600

    Grammer Corrections in Answer of Questions

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ##  Netlify: [Live Site Link](https://shahariar-assignment-9.netlify.app/).
 
 ### Description:
-* Home Section: Home section have heading and Recent 3 customer reviews. 
-* Review Section: Review section have all customer reviews with their name and ratings.
-* Dashboard: Dashboard have 4 kind of chart using rechart.
-* Blog: Description about Context API, Semantic Tag and difference between Inline, Block and Inline-block.
+* Home Section: Home section have heading and Recent 3 customer's reviews. 
+* Review Section: Review section have all customers reviews with their name and ratings.
+* Dashboard: Dashboard have 4 kind of chart using rechart API.
+* Blog: Blog have description about Context API, Semantic Tag and difference between Inline, Block and Inline-block.
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -10,10 +10,10 @@ const Blogs = () => {
                     <p className='font-semibold'>Answer:</p>
                     <ul>
                         <li>Context API used to share data to child componenet without prop-drilling.</li>
-                        <li>Context API export variables that can be accessable in all levels of your application.</li>
+                        <li>Context API export variables that can be accessable in all levels in child.</li>
                         <li>useContext hook used to access parent componenets provided value without sharing the value to other componenet.</li>
                         <li>Using useContext we can select the provider so data privacy not compromised.</li>
-                        <li>Context api is much helpfull when data to share in a largest nested componenet.</li>
+                        <li>Context api is very helpfull when we share data in a nested componenet.</li>
                     </ul>
                 </div>
                 <div>
@@ -34,12 +34,12 @@ const Blogs = () => {
                     <p className='font-semibold mt-4'>Difference Between Inline vs Block vs Inline-block.</p>
                     <p className='font-semibold'>Answer:</p>
                     <ul>
-                        <li>Inline: Element doesn't start on a new line.It takes width it's required.</li>
-                        <li>Inline: we can't set it's width and height.</li>
-                        <li>Inline: we can't set vertical margin and padding.</li>
+                        <li>Inline: Element doesn't start on a new line.It takes width as much as it required.</li>
+                        <li>Inline: We can't set it's width and height.</li>
+                        <li>Inline: We can't set vertical margin and padding.</li>
                         <li>block: ELement start on a new line. It takes full avaliable width.</li>
-                        <li>block: we can set it's height, width and vertical margin, padding.</li>
-                        <li>Inline-block: ELement doesn't start on a new line. It takes width it's required.</li>
+                        <li>block: We can set it's height, width and vertical margin, padding.</li>
+                        <li>Inline-block: ELement doesn't start on a new line. It takes width as much as it required.</li>
                         <li>Inline-block: we can set it's height, width and vertical margin, padding.</li>
                         <li>Inline-block: It is formatted like inline element but behave like block element.</li>
                     </ul>
